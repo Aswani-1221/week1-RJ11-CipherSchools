3435a4a283077a260e21d3ffced6e58d5cc2f552
Aswani kumari <115870454+Aswani-1221@users.noreply.github.com>
2023-01-27 17:49:43 +0530

    Add files via upload

---
+++ string interpolation/java string interpolation.js
@@ -0,0 +1,61 @@
+//using let
+//using var
+//using const
+
+// const myName =[];
+// console.log(myName);
+// myName.append("John");
+// console.log(myName);
+// const muName = 5;
+
+
+// let myName ="John"
+// console.log(myName);
+// myName = "Alex";
+// console.log(myName);
+
+//string interpolation
+
+        //String  interpolation
+        // var age =30;
+        // var name= "david"
+        // console.log(`your  name is ${name}  and age is ${age}` )
+
+        //switch case
+
+        // var points = prompt("enter the points")
+        // switch (points){
+        //     case 1:
+        //         alert(" you lost the match")
+        //         break;
+
+        //      case 5:
+        //         alert(" you win the match")
+        //         break;
+
+        //     default:
+        //         alert(" restart the match")
+        // }
+
+        //for loops
+
+        // let x=1
+        // for(1; x<=50 ; x++){
+        //     console.log(x)
+        // }
+
+        // function myInfo(fname, lname, country) {
+        //     return "My name is " + fname + " " + lname + ". " 
+        //               + country + " is my favorite country."; 
+        // }
+        // console.log(myInfo("john", "doe", "India"));
+
+
+
+        //functions
+
+//         let x = myFunction(4, 3);   // Function is called, return value will end up in x
+
+// function myFunction(a, b) {
+//   return a * b;             // Function returns the product of a and b
+// }
